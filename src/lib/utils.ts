1d0e0a451a75cd0028794121d72aed8c5ad3fcde
thebigthing313 <adriankabigting@gmail.com>
2025-10-12 11:43:45 -0400

    feat: add Prettify utility type for improved type readability

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/**
+ * Utility type to simplify and "flatten" a given type `T`.
+ * It reconstructs the type by mapping over its properties,
+ * removing intersections and making the resulting type easier to read in editor tooltips.
+ *
+ * @template T - The type to prettify.
+ * @example
+ * type A = { a: number } & { b: string };
+ * type B = Prettify<A>; // { a: number; b: string }
+ */
+export type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
