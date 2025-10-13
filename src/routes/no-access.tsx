165a3fdf4060da7db48aa1ab167f09d465ebdbe4
thebigthing313 <adriankabigting@gmail.com>
2025-10-13 06:18:02 -0400

    feat: add no-access, privacy-policy, and terms-of-service routes

---
+++ src/routes/no-access.tsx
@@ -0,0 +1,21 @@
+import { createFileRoute, Link } from '@tanstack/react-router'
+import { Typography } from '@/components/typography'
+
+export const Route = createFileRoute('/no-access')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return (
+    <div className="max-w-lg flex flex-col gap-2">
+      <Typography tag="h3">Access Denied</Typography>
+      <Typography tag="h4">
+        Sorry you do not have access to this group.
+      </Typography>
+      <Typography tag="h4">
+        Please contact the group administrator for an invite.
+      </Typography>
+      <Link to="/select-group">Select Another Group</Link>
+    </div>
+  )
+}
---
+++ src/routes/privacy-policy.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/privacy-policy')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <div>Hello "/_app/privacy-policy"!</div>
+}
