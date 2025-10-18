ab0ecee87edb0785ea5b4232bc858610f9d28b69
thebigthing313 <adriankabigting@gmail.com>
2025-10-18 12:08:35 -0400

    refactor: remove unused QueryDefinition and DataDefinition types from data-types.ts

--- src/types/data-types.ts
+++ src/types/data-types.ts
@@ -1,5 +1,4 @@
-import { QueryOptions } from '@tanstack/react-query'
-import { Database } from '@/types/supabase-types'
+import type { Database } from '@/db/supabase-types'
 
 export type Table = keyof Database['public']['Tables']
 export type Row<T extends Table> = Database['public']['Tables'][T]['Row']
@@ -7,15 +6,3 @@ export type Insert<T extends Table> = Database['public']['Tables'][T]['Insert']
 export type Update<T extends Table> = Database['public']['Tables'][T]['Update']
 export type Enum = keyof Database['public']['Enums']
 export type EnumValue<T extends Enum> = Database['public']['Enums'][T]
-
-export type QueryDefinition = {
-  id: string
-  key: any
-  fetcher: () => Promise<any>
-  queryOptions: QueryOptions
-}
-export type DataDefinition = {
-  table: Table
-  queries: Array<QueryDefinition>
-  fields: keyof Row<Table>
-}
