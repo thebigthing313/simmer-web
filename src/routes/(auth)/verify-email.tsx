65e21e41ac49b9dbba8be50328a9bb95c48cf43a
thebigthing313 <adriankabigting@gmail.com>
2025-10-18 12:13:25 -0400

    refactor: standardize formatting and improve consistency in verify-email component

--- src/routes/(auth)/verify-email.tsx
+++ src/routes/(auth)/verify-email.tsx
@@ -1,21 +1,21 @@
-import { createFileRoute, Link } from '@tanstack/react-router';
-import { FormLayout } from './-components/form-layout';
+import { Link, createFileRoute } from '@tanstack/react-router'
+import { FormLayout } from './-components/form-layout'
 
 export const Route = createFileRoute('/(auth)/verify-email')({
-  component: RouteComponent
-});
+  component: RouteComponent,
+})
 
 function RouteComponent() {
   return (
     <FormLayout
-      title='Thank you for joining SIMMER!'
-      description='Please check your email to verify your account.'
+      title="Thank you for joining SIMMER!"
+      description="Please check your email to verify your account."
     >
-      <div className='flex w-full justify-center'>
-        <Link className='hover:underline' to='/login'>
+      <div className="flex w-full justify-center">
+        <Link className="hover:underline" to="/login">
           Return to Login Page
         </Link>
       </div>
     </FormLayout>
-  );
+  )
 }
