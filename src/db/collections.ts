a9a50e4764d4d27c76605652cf05f83362362d81
thebigthing313 <adriankabigting@gmail.com>
2025-10-19 16:36:26 -0400

    refactor: add collections for groups, profiles, group_profiles, and group_invites

---
+++ src/db/collections.ts
@@ -0,0 +1,17 @@
+import { createCollection } from '@tanstack/react-db'
+import { DBWholeCollectionOptions } from '@/db/generic-collection-options'
+
+export const groupsCollection = createCollection(
+  DBWholeCollectionOptions('groups', 1000 * 60 * 60),
+) // 1 hour
+export const profilesCollection = createCollection(
+  DBWholeCollectionOptions('profiles', 1000 * 60 * 60),
+) // 1 hour
+
+export const groupProfilesCollection = createCollection(
+  DBWholeCollectionOptions('group_profiles', 1000 * 60 * 30),
+) // 30 minutes
+
+export const groupInvitesCollection = createCollection(
+  DBWholeCollectionOptions('group_invites', 1000 * 60 * 30),
+) // 30 minutes
