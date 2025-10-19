f723fcbe25a6c4dcdcb79e12c0e6b6a02492278b
thebigthing313 <adriankabigting@gmail.com>
2025-10-19 19:16:07 -0400

    refactor: simplify profile data retrieval and improve welcome message rendering

--- src/routes/(user)/index.tsx
+++ src/routes/(user)/index.tsx
@@ -1,28 +1,22 @@
-import { Typography } from '@/components/typography'
 import { createFileRoute } from '@tanstack/react-router'
 import { MyGroupsCard } from './-components/my-groups-card'
 import { MyInvitesCard } from './-components/my-invites-card'
-import { eq, useLiveQuery } from '@tanstack/react-db'
-import { profilesCollection } from '@/collections/profiles'
+import { Typography } from '@/components/typography'
+import { useProfile } from '@/db/hooks/use-profile'
 
 export const Route = createFileRoute('/(user)/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  const { supabase, queryClient, profile_id } = Route.useRouteContext()
-  const { data } = useLiveQuery((q) =>
-    q
-      .from({ profile: profilesCollection(supabase, queryClient) })
-      .where(({ profile }) => eq(profile.id, profile_id))
-      .select(({ profile }) => ({
-        first_name: profile.first_name,
-      })),
-  )
+  const { profile_id } = Route.useRouteContext()
+  const { query } = useProfile(profile_id)
+  const profile = query.data[0]
+
   return (
     <div className="flex flex-col max-w-lg gap-2">
       <Typography tag="h4" className="font-semibold">
-        Welcome back, {data[0].first_name}!
+        Welcome back, {profile.first_name}!
       </Typography>
 
       <MyGroupsCard />
