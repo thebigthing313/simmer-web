8a822f4c5b2b0468c3b7898ed5502c316b582a39
thebigthing313 <adriankabigting@gmail.com>
2025-10-19 19:14:59 -0400

    refactor: reorder import statements and simplify code formatting in route.tsx

--- src/routes/$groupSlug/route.tsx
+++ src/routes/$groupSlug/route.tsx
@@ -1,16 +1,16 @@
-import { createFileRoute, notFound, Outlet } from '@tanstack/react-router';
-import { useGroupStore } from '../../stores/group-stores';
+import { Outlet, createFileRoute, notFound } from '@tanstack/react-router'
+import { useGroupStore } from '../../stores/group-stores'
 
 export const Route = createFileRoute('/$groupSlug')({
   beforeLoad: async ({ params }) => {
-    const { groupSlug } = params;
-    const groupId = await useGroupStore.getState().beforeLoad(groupSlug);
-    if (!groupId) throw notFound();
-    return { groupId };
+    const { groupSlug } = params
+    const groupId = await useGroupStore.getState().beforeLoad(groupSlug)
+    if (!groupId) throw notFound()
+    return { groupId }
   },
-  component: RouteComponent
-});
+  component: RouteComponent,
+})
 
 function RouteComponent() {
-  return <Outlet />;
+  return <Outlet />
 }
