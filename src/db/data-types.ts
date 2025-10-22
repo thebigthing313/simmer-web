8216e5c4f6256aeb218d5f660c250d417c8e7c4f
thebigthing313 <adriankabigting@gmail.com>
2025-10-22 19:20:52 -0400

    feat: add validation schemas for email, address, name, password, phone number, and URL refactor: remove obsolete data-types.ts file and consolidate type definitions in data-types.ts

--- src/db/data-types.ts
+++ src/db/data-types.ts
@@ -7,3 +7,14 @@ export type Update<T extends Table> = Database['public']['Tables'][T]['Update']
 
 export type Enum = keyof Database['public']['Enums']
 export type EnumValue<T extends Enum> = Database['public']['Enums'][T]
+
+type TransformedRow<T> = {
+  [K in keyof T]: // Check if the key ends with '_at' or '_date' AND the original type is string
+  K extends `${infer _P}_at` | `${infer _P}_date`
+    ? T[K] extends string
+      ? Date
+      : T[K] // If it matches and is a string, make it Date
+    : T[K] // Otherwise, keep the original type
+}
+
+export type AppRow<T extends Table> = TransformedRow<Row<T>>
--- src/types/data-types.ts
+++
@@ -1,8 +0,0 @@
-import type { Database } from '@/db/supabase-types'
-
-export type Table = keyof Database['public']['Tables']
-export type Row<T extends Table> = Database['public']['Tables'][T]['Row']
-export type Insert<T extends Table> = Database['public']['Tables'][T]['Insert']
-export type Update<T extends Table> = Database['public']['Tables'][T]['Update']
-export type Enum = keyof Database['public']['Enums']
-export type EnumValue<T extends Enum> = Database['public']['Enums'][T]
