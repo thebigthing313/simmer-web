2e45ce6c3b5089fa584e34e527c4ac0082b84aff
thebigthing313 <adriankabigting@gmail.com>
2025-10-27 04:18:18 -0400

    feat: add GroupCard and GroupCardSkeleton components for displaying group information

---
+++ src/components/blocks/group-item.tsx
@@ -0,0 +1,100 @@
+import { parsePhoneNumber } from 'libphonenumber-js/min';
+import {
+	Item,
+	ItemContent,
+	ItemDescription,
+	ItemGroup,
+	ItemMedia,
+	ItemTitle,
+} from '@/components/ui/item';
+import { cn } from '@/lib/utils';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import { Skeleton } from '../ui/skeleton';
+
+/**
+ * Props for the GroupCard component.
+ */
+interface GroupCardProps {
+	/** The group data object containing logo, name, address, and phone. If undefined, renders a skeleton. */
+	group?: {
+		/** URL of the group's logo (optional). */
+		logo_url?: string;
+		/** Name of the group. */
+		group_name: string;
+		/** Address of the group. */
+		address: string;
+		/** Phone number of the group. */
+		phone: string;
+	};
+}
+
+export function GroupCard({
+	group,
+	className,
+	ref,
+}: GroupCardProps & React.ComponentPropsWithRef<'div'>) {
+	if (!group) {
+		return <GroupCardSkeleton />;
+	}
+
+	const { logo_url, group_name, address, phone } = group;
+
+	const parsedPhone = parsePhoneNumber(phone, 'US').formatNational();
+
+	return (
+		<Item
+			key={`group-card-${group_name}`}
+			variant="outline"
+			className={cn('min-w-xs max-w-md bg-card', className)}
+			ref={ref}
+		>
+			<ItemMedia className="size-20 bg-card" variant="image">
+				<Avatar className="size-20">
+					<AvatarImage src={logo_url} alt={group_name} />
+					<AvatarFallback>{group_name.charAt(0).toUpperCase()}</AvatarFallback>
+				</Avatar>
+			</ItemMedia>
+
+			<ItemContent>
+				<ItemTitle>{group_name}</ItemTitle>
+				<ItemDescription className="tracking-tight">{address}</ItemDescription>
+				<ItemDescription className="tracking-tight">
+					{parsedPhone}
+				</ItemDescription>
+			</ItemContent>
+		</Item>
+	);
+}
+
+function GroupCardSkeleton() {
+	return (
+		<Item variant="outline" className="min-w-xs max-w-md bg-card">
+			<ItemMedia className="size-20 bg-background" variant="image">
+				<Skeleton className="w-full h-full rounded-full" />
+			</ItemMedia>
+
+			<ItemContent>
+				<Skeleton className="h-4 w-3/4" />
+				<Skeleton className="h-4 w-5/6" />
+				<Skeleton className="h-4 w-1/2" />
+			</ItemContent>
+		</Item>
+	);
+}
+
+export function GroupCardGroup({
+	className,
+	children,
+	ref,
+	...props
+}: React.ComponentPropsWithRef<'div'>) {
+	return (
+		<ItemGroup
+			className={cn('min-w-xs max-w-md', className)}
+			{...props}
+			ref={ref}
+		>
+			{children}
+		</ItemGroup>
+	);
+}
