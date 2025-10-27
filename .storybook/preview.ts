c69453540bf4594cfa157aa83f31002f645afc31
thebigthing313 <adriankabigting@gmail.com>
2025-10-27 07:29:11 -0400

    refactor: update import statements and formatting in Checkbox and ButtonGroup components for consistency

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react-vite'
+import { IndexEntry } from 'storybook/internal/types';
 import '../src/styles.css'
 
 const preview: Preview = {
@@ -9,7 +10,11 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
-  },
-}
+      // The `a` and `b` arguments in this function have a type of `import('storybook/internal/types').IndexEntry`. Remember that the function is executed in a JavaScript environment, so use JSDoc for IntelliSense to introspect it.
+      storySort: (a: IndexEntry, b: IndexEntry) =>
+        a.id === b.id ? 0 : a.id.localeCompare(b.id, undefined, { numeric: true }),
+    },
+  }
+
 
 export default preview
