bcc00b8eb9b2d76119d9e90b2e8dbdab35d1fe03
thebigthing313 <adriankabigting@gmail.com>
2025-10-27 04:54:36 -0400

    refactor: enhance NotFound component to accept className prop for improved styling flexibility

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -1,12 +1,17 @@
+import { cn } from '@/lib/utils';
 import { H1, H2 } from './typography';
 import { Button } from './ui/button';
 
 interface NotFoundProps {
 	onGoHome?: () => void;
 }
-export function NotFound({ onGoHome }: NotFoundProps) {
+
+export function NotFound({
+	className,
+	onGoHome,
+}: NotFoundProps & React.ComponentPropsWithRef<'main'>) {
 	return (
-		<main className="max-w-lg grid grid-rows gap-2">
+		<main className={cn('max-w-lg grid grid-rows gap-2', className)}>
 			<H1>404 - Page Not Found</H1>
 			<H2>Sorry, the page you are looking for does not exist.</H2>
 
