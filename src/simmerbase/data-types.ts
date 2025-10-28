2b886f2b5be032dfbc303fb0495c77f663eafd07
thebigthing313 <adriankabigting@gmail.com>
2025-10-28 18:59:37 -0400

    feat: extend TransformedRow type for additional table mappings

--- src/simmerbase/data-types.ts
+++ src/simmerbase/data-types.ts
@@ -1,3 +1,5 @@
+import type { Tables } from './supabase-types';
+
 export type TransformedRow<T> = {
 	[K in keyof T]: K extends `${string}_at` | `${string}_date`
 		? T[K] extends string
@@ -5,3 +7,9 @@ export type TransformedRow<T> = {
 			: T[K]
 		: T[K];
 };
+
+export type Group = TransformedRow<Tables<'groups'>>;
+export type GroupProfile = TransformedRow<Tables<'group_profiles'>>;
+export type GroupInvite = TransformedRow<Tables<'group_invites'>>;
+export type Region = TransformedRow<Tables<'regions'>>;
+export type Profile = TransformedRow<Tables<'profiles'>>;
