af71fcf3508075c586541b894694b038f48b741a
thebigthing313 <adriankabigting@gmail.com>
2025-10-29 00:07:57 -0400

    feat: refactor date transformation logic for database rows

--- src/simmerbase/data-types.ts
+++ src/simmerbase/data-types.ts
@@ -1,15 +1,40 @@
-import type { Tables } from './supabase-types';
+import type {
+	Database,
+	Tables,
+	TablesInsert,
+	TablesUpdate,
+} from './supabase-types';
 
-export type TransformedRow<T> = {
-	[K in keyof T]: K extends `${string}_at` | `${string}_date`
-		? T[K] extends string
-			? Date
-			: T[K]
-		: T[K];
+type TransformDate<U> = U extends string
+	? Date
+	: U extends string | null
+		? Date | null
+		: U extends string | undefined
+			? Date | undefined
+			: U extends string | null | undefined
+				? Date | null | undefined
+				: U;
+
+export type Table = keyof Database['public']['Tables'];
+
+export type Row<T extends Table> = {
+	[K in keyof Tables<T>]: K extends `${string}_at` | `${string}_date`
+		? TransformDate<Tables<T>[K]>
+		: Tables<T>[K];
+};
+export type InsertRow<T extends Table> = {
+	[K in keyof TablesInsert<T>]: K extends `${string}_at` | `${string}_date`
+		? TransformDate<TablesInsert<T>[K]>
+		: TablesInsert<T>[K];
+};
+export type UpdateRow<T extends Table> = {
+	[K in keyof TablesUpdate<T>]: K extends `${string}_at` | `${string}_date`
+		? TransformDate<TablesUpdate<T>[K]>
+		: TablesUpdate<T>[K];
 };
 
-export type Group = TransformedRow<Tables<'groups'>>;
-export type GroupProfile = TransformedRow<Tables<'group_profiles'>>;
-export type GroupInvite = TransformedRow<Tables<'group_invites'>>;
-export type Region = TransformedRow<Tables<'regions'>>;
-export type Profile = TransformedRow<Tables<'profiles'>>;
+export type AssertEqual<T, Expected> = T extends Expected
+	? Expected extends T
+		? true
+		: false
+	: false;
