a87adfbeda445515a7c284d08328ff35568af934
thebigthing313 <adriankabigting@gmail.com>
2025-10-30 12:08:31 -0400

    feat: add TypeScript definitions for environment variables

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,22 @@
 /// <reference types="vite/client" />
-/// <reference types="vite-plugin-svgr/client" />
 
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string;
   readonly VITE_SUPABASE_SBPUBLISHABLE_KEY: string;
+
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      readonly SUPABASE_SERVICE_ROLE_KEY: string;
+      readonly NODE_ENV: 'development' | 'production' | 'test';
+    }
+  }
+}
+
+export {}
