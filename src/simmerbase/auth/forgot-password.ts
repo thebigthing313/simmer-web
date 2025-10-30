39298979dd428288c0fd6bd55462c16c62423e52
thebigthing313 <adriankabigting@gmail.com>
2025-10-30 09:28:00 -0400

    feat: implement forgot password functionality with input validation ssr

--- src/simmerbase/auth/forgot-password.ts
+++ src/simmerbase/auth/forgot-password.ts
@@ -1,9 +1,21 @@
+import { createServerFn } from '@tanstack/react-start';
+import z from 'zod';
 import { supabase } from '@/simmerbase/client';
 
-export async function forgotPassword(email: string): Promise<void> {
-	const { error } = await supabase.auth.resetPasswordForEmail(email, {
-		redirectTo: `${window.location.origin}/reset-password`,
-	});
+export async function forgotPassword2(email: string): Promise<void> {}
 
-	if (error) throw error;
-}
+const ForgottenPasswordSchema = z.object({
+	email: z.email(),
+});
+
+export const forgotPassword = createServerFn({ method: 'POST' })
+	.inputValidator(ForgottenPasswordSchema)
+	.handler(async ({ data }) => {
+		const { error } = await supabase.auth.resetPasswordForEmail(data.email, {
+			redirectTo: `${window.location.origin}/reset-password`,
+		});
+
+		if (error) {
+			return { error: true, message: error.message };
+		}
+	});
