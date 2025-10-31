a9b31d608f2c58e86d438d2d7e88f4d276ca830d
thebigthing313 <adriankabigting@gmail.com>
2025-10-30 22:03:17 -0400

    feat: update confirm-email route to include FormLayout and navigation link

--- src/routes/(auth)/confirm-email.tsx
+++ src/routes/(auth)/confirm-email.tsx
@@ -1,9 +1,21 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { FormLayout } from './-components/form-layout';
 
 export const Route = createFileRoute('/(auth)/confirm-email')({
-  component: RouteComponent,
-})
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/(auth)/confirm-email"!</div>
+	return (
+		<FormLayout
+			title="Thank you for joining SIMMER!"
+			description="Please check your email to verify your account."
+		>
+			<div className="flex w-full justify-center">
+				<Link className="hover:underline" to="/login">
+					Return to Login Page
+				</Link>
+			</div>
+		</FormLayout>
+	);
 }
