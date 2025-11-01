88f96419ecc98c180d574d3ff1daca6223dcea0c
thebigthing313 <adriankabigting@gmail.com>
2025-11-01 10:15:45 -0400

    feat: implement SharedLayout and sidebar components for improved navigation structure

---
+++ src/components/layout/shared-layout.tsx
@@ -0,0 +1,86 @@
+import { LogOut } from 'lucide-react';
+import type React from 'react';
+import { ThemeModeToggle } from '../blocks/theme-mode-toggle';
+import { UserButton } from '../blocks/user-button';
+import { Separator } from '../ui/separator';
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarHeader,
+	SidebarInset,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarProvider,
+	SidebarRail,
+	SidebarTrigger,
+} from '../ui/sidebar';
+import { GroupButton } from './group-button';
+
+export function SharedLayout({ children }: React.ComponentPropsWithRef<'div'>) {
+	return <SidebarProvider>{children}</SidebarProvider>;
+}
+
+interface SharedSidebarProps {
+	groupProps?: React.ComponentPropsWithoutRef<typeof GroupButton>;
+	onSignOut: () => void;
+}
+export function SharedSidebar({
+	groupProps,
+	onSignOut,
+	children,
+}: SharedSidebarProps & React.ComponentPropsWithRef<typeof SidebarContent>) {
+	return (
+		<Sidebar collapsible="icon">
+			<SidebarHeader>
+				{groupProps && <GroupButton {...groupProps} />}
+			</SidebarHeader>
+			<SidebarContent className="border-b border-t">{children}</SidebarContent>
+			<SidebarFooter>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton
+							type="button"
+							variant="default"
+							onClick={onSignOut}
+						>
+							<LogOut /> <span>Sign Out</span>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarFooter>
+			<SidebarRail />
+		</Sidebar>
+	);
+}
+
+interface SharedSidebarInsetProps {
+	userButtonProps?: React.ComponentPropsWithoutRef<typeof UserButton>;
+}
+
+export function SharedSidebarInset({
+	userButtonProps,
+	children,
+}: SharedSidebarInsetProps & React.ComponentPropsWithRef<'div'>) {
+	return (
+		<SidebarInset>
+			<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+				<div className="flex w-full justify-between">
+					<div className="flex items-center gap-2 px-4">
+						<SidebarTrigger className="-ml-1" />
+						<Separator
+							orientation="vertical"
+							className="mr-2 data-[orientation=vertical]:h-4"
+						/>
+					</div>
+					<div className="flex items-center gap-2 px-4">
+						<ThemeModeToggle />
+						<UserButton {...userButtonProps} />
+					</div>
+				</div>
+			</header>
+			<div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+		</SidebarInset>
+	);
+}
