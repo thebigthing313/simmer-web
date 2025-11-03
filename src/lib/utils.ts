2fd91f2a35a765fc2af49ad4d1b111f9ff5b71f1
thebigthing313 <adriankabigting@gmail.com>
2025-11-02 21:28:41 -0500

    feat: add buildChangeSet utility function for tracking changes between objects

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,20 @@ export function capitalize(str: string): string {
 	if (str.length === 0) return str;
 	return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function buildChangeSet<T extends object>(
+	defaultValues: T,
+	newValues: T,
+) {
+	const changes: Partial<T> = {};
+
+	for (const key in defaultValues) {
+		if (Object.hasOwn(defaultValues, key)) {
+			if (defaultValues[key] !== newValues[key]) {
+				changes[key] = newValues[key];
+			}
+		}
+	}
+
+	return changes;
+}
