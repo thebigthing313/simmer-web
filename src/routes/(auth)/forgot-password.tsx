99d52ad63221025d41f15183c5bb30ff37610aa4
thebigthing313 <adriankabigting@gmail.com>
2025-11-02 21:26:42 -0500

    refactor forgot password route to integrate form handling directly and enhance UI components

--- src/routes/(auth)/forgot-password.tsx
+++ src/routes/(auth)/forgot-password.tsx
@@ -1,9 +1,18 @@
 import { useMutation } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
 import { useState } from 'react';
+import z from 'zod';
+import {
+	FieldDescription,
+	FieldGroup,
+	FieldLegend,
+	FieldSet,
+} from '@/components/ui/field';
+import { useAppForm } from '@/forms/form-context';
 import { forgotPassword } from '@/simmerbase/auth/forgot-password';
+import { EmailSchema } from '@/simmerbase/schemas/fields';
 import { ErrorAlert } from './-components/error-alert';
-import { ForgotPasswordForm } from './-components/forgot-password-form';
+import { FormLayout } from './-components/form-layout';
 
 export const Route = createFileRoute('/(auth)/forgot-password')({
 	component: RouteComponent,
@@ -20,16 +29,49 @@ function RouteComponent() {
 		},
 	});
 
+	const ForgotPasswordSchema = z.object({
+		email: EmailSchema,
+	});
+
+	const form = useAppForm({
+		validators: { onChange: ForgotPasswordSchema },
+		defaultValues: { email: '' },
+		onSubmit: async ({ value }) =>
+			forgotPasswordMutation.mutate({ data: { email: value.email } }),
+	});
+
 	return (
 		<>
 			{errorMsg && (
 				<ErrorAlert errorTitle="Forgot Password Error" errorMsg={errorMsg} />
 			)}
-			<ForgotPasswordForm
-				handleSubmit={(email: string) =>
-					forgotPasswordMutation.mutate({ data: { email: email } })
-				}
-			/>
+			<FormLayout showReturnToLogin>
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						e.stopPropagation();
+						form.handleSubmit();
+					}}
+				>
+					<FieldSet>
+						<FieldLegend>Forgot Password</FieldLegend>
+						<FieldDescription>
+							You will receive an e-mail with a link to reset your password.
+						</FieldDescription>
+						<FieldGroup>
+							<form.AppField name="email">
+								{(field) => <field.TextField label="Email" />}
+							</form.AppField>
+							<form.AppForm>
+								<form.SubmitFormButton
+									label="Send Reset Link"
+									className="w-full"
+								/>
+							</form.AppForm>
+						</FieldGroup>
+					</FieldSet>
+				</form>
+			</FormLayout>
 		</>
 	);
 }
