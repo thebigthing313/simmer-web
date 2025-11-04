5105c4ccf1e2490f41ee8ea85ae08c8940c5c224
thebigthing313 <adriankabigting@gmail.com>
2025-11-04 11:07:09 -0500

    feat: add initial storage bucket setup and access policies for avatars and group images

---
+++ supabase/seed/000 - storage_buckets.sql
@@ -0,0 +1,56 @@
+insert into
+    storage.buckets (id, name, public)
+values
+    ('avatars', 'avatars', true),
+    ('group_images', 'group_images', true)
+on conflict (id) do nothing;
+
+CREATE POLICY "Users can manage their own avatars 1oj01fe_0"
+ON "storage"."objects"
+FOR INSERT
+TO "authenticated"
+WITH CHECK
+    ((("bucket_id" = 'avatars'::"text")
+    AND ("owner_id" = (( SELECT "auth"."uid"() AS "uid"))::"text")));
+
+
+
+CREATE POLICY "Users can manage their own avatars 1oj01fe_1"
+ON "storage"."objects"
+FOR UPDATE
+TO "authenticated"
+USING
+    ((("bucket_id" = 'avatars'::"text")
+    AND ("owner_id" = (( SELECT "auth"."uid"() AS "uid"))::"text")));
+
+
+CREATE POLICY "Users can manage their own avatars 1oj01fe_2"
+ON "storage"."objects"
+FOR DELETE TO "authenticated"
+USING
+    ((("bucket_id" = 'avatars'::"text")
+    AND ("owner_id" = (( SELECT "auth"."uid"() AS "uid"))::"text")));
+
+CREATE POLICY "Users can manage their own groups images 1qwv6t4_0"
+ON "storage"."objects"
+FOR INSERT
+TO "authenticated"
+WITH CHECK
+    ((("bucket_id" = 'group_images'::"text")
+    AND "public"."user_is_group_member"((("storage"."foldername"("name"))[1])::"uuid")));
+
+CREATE POLICY "Users can manage their own groups images 1qwv6t4_1"
+ON "storage"."objects"
+FOR UPDATE
+TO "authenticated"
+USING
+    ((("bucket_id" = 'group_images'::"text")
+    AND "public"."user_is_group_member"((("storage"."foldername"("name"))[1])::"uuid")));
+
+CREATE POLICY "Users can manage their own groups images 1qwv6t4_2"
+ON "storage"."objects"
+FOR DELETE
+TO "authenticated"
+USING
+    ((("bucket_id" = 'group_images'::"text")
+    AND "public"."user_is_group_member"((("storage"."foldername"("name"))[1])::"uuid")));
