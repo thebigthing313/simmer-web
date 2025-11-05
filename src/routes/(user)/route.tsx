4439a0b212b70b60301b7d5456010fea08f7930c
thebigthing313 <adriankabigting@gmail.com>
2025-11-04 19:21:50 -0500

    feat: update user route to include profile data and improve navigation

--- src/routes/(user)/route.tsx
+++ src/routes/(user)/route.tsx
@@ -10,20 +10,24 @@ import {
 	SharedSidebarInset,
 } from '@/components/layout/shared-layout';
 import { getClaims } from '@/simmerbase/auth/get-claims';
+import { useProfile } from '@/simmerbase/db/hooks/use-profile';
 import { SidebarContents } from './-components/sidebar-contents';
 
 export const Route = createFileRoute('/(user)')({
 	beforeLoad: async () => {
 		const claims = await getClaims();
 		const { user_id, user_email, profile_id, groups } = claims;
-		if (!user_id || !profile_id) throw redirect({ to: '/' });
+		if (!user_id || !profile_id) throw redirect({ to: '/login' });
 		return { user_id, user_email, profile_id, groups };
 	},
 	component: RouteComponent,
 });
 
 function RouteComponent() {
+	const { profile_id } = Route.useRouteContext();
 	const navigate = useNavigate();
+	const { data } = useProfile(profile_id);
+	if (!data) return null;
 	return (
 		<SharedLayout>
 			<SharedSidebar groupProps={{ onClick: () => {} }}>
@@ -31,6 +35,8 @@ function RouteComponent() {
 			</SharedSidebar>
 			<SharedSidebarInset
 				userButtonProps={{
+					imageUrl: data.avatar_url || undefined,
+					fallback: `${data.first_name?.[0]}${data.last_name?.[0]}`,
 					onClick: () => {
 						navigate({ to: '/' });
 					},
