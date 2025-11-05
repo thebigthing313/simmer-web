ca292e85c21998aa2ae52183295c1f56b197a7aa
thebigthing313 <adriankabigting@gmail.com>
2025-11-04 19:20:08 -0500

    feat: add SUPABASE_URL to ProcessEnv interface in env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -14,6 +14,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       readonly SUPABASE_SERVICE_ROLE_KEY: string;
+      readonly SUPABASE_URL: string;
       readonly NODE_ENV: 'development' | 'production' | 'test';
     }
   }
