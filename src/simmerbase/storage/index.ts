7c9c6e675ae74093c04c3ffae4a88a956c30092d
thebigthing313 <adriankabigting@gmail.com>
2025-11-04 19:23:55 -0500

    refactor: remove unused export statements for updateAvatar and updateGroupImage

--- src/simmerbase/storage/index.ts
+++ src/simmerbase/storage/index.ts
@@ -6,12 +6,7 @@
 export { compressImage, compressImageToSize } from './compress-image';
 // Delete operations
 export { deleteFile, deleteFiles } from './delete-image';
-export { updateAvatar } from './update-avatar';
-export { updateGroupImage } from './update-group-image';
 // Avatar operations
 export { uploadAvatar } from './upload-avatar';
 // Group image operations
 export { uploadGroupImage } from './upload-group-image';
-
-// Legacy upload (keep for backwards compatibility)
-export { uploadPhoto } from './upload-image';
