0912925a078b9783014a003d03552f7fd46fe4bc
thebigthing313 <adriankabigting@gmail.com>
2025-11-05 23:43:30 -0500

    feat: add PhoneField and AutocompleteField to form context field components

--- src/forms/form-context.ts
+++ src/forms/form-context.ts
@@ -1,6 +1,8 @@
 import { createFormHook, createFormHookContexts } from '@tanstack/react-form';
+import { AutocompleteField } from './autocomplete-field';
 import { AvatarField } from './avatar-field';
 import { PasswordField } from './password-field';
+import { PhoneField } from './phone-field';
 import { ResetFormButton } from './reset-form-button';
 import { StyledTextField } from './styled-text-field';
 import { SubmitFormButton } from './submit-form-button';
@@ -12,6 +14,13 @@ export const { fieldContext, formContext, useFieldContext, useFormContext } =
 export const { useAppForm, withFieldGroup } = createFormHook({
 	fieldContext,
 	formContext,
-	fieldComponents: { TextField, PasswordField, StyledTextField, AvatarField },
+	fieldComponents: {
+		TextField,
+		PasswordField,
+		StyledTextField,
+		AvatarField,
+		PhoneField,
+		AutocompleteField,
+	},
 	formComponents: { SubmitFormButton, ResetFormButton },
 });
