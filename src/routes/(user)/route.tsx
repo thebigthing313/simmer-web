4c3cf9932ced0514b5d622436a879ee8208da1f6
thebigthing313 <adriankabigting@gmail.com>
2025-11-08 10:11:00 -0500

    feat: add error boundary and loading spinner to user route component

--- src/routes/(user)/route.tsx
+++ src/routes/(user)/route.tsx
@@ -4,11 +4,14 @@ import {
 	redirect,
 	useNavigate,
 } from '@tanstack/react-router';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import {
 	SharedLayout,
 	SharedSidebar,
 	SharedSidebarInset,
 } from '@/components/layout/shared-layout';
+import { Spinner } from '@/components/ui/spinner';
 import { getClaims } from '@/simmerbase/auth/get-claims';
 import { useProfile } from '@/simmerbase/db/hooks/use-profile';
 import { SidebarContents } from './-components/sidebar-contents';
@@ -20,14 +23,20 @@ export const Route = createFileRoute('/(user)')({
 		if (!user_id || !profile_id) throw redirect({ to: '/login' });
 		return { user_id, user_email, profile_id, groups };
 	},
-	component: RouteComponent,
+	component: () => (
+		<ErrorBoundary fallback={<div>Something went wrong.</div>}>
+			<Suspense fallback={<AppLoading />}>
+				<RouteComponent />
+			</Suspense>
+		</ErrorBoundary>
+	),
 });
 
 function RouteComponent() {
 	const { profile_id } = Route.useRouteContext();
 	const navigate = useNavigate();
 	const { data } = useProfile(profile_id);
-	if (!data) return null;
+
 	return (
 		<SharedLayout>
 			<SharedSidebar groupProps={{ onClick: () => {} }}>
@@ -36,7 +45,7 @@ function RouteComponent() {
 			<SharedSidebarInset
 				userButtonProps={{
 					imageUrl: data.avatar_url || undefined,
-					fallback: `${data.first_name?.[0]}${data.last_name?.[0]}`,
+					fallback: `${data.first_name[0]}${data.last_name[0]}`,
 					onClick: () => {
 						navigate({ to: '/' });
 					},
@@ -45,5 +54,21 @@ function RouteComponent() {
 				<Outlet />
 			</SharedSidebarInset>
 		</SharedLayout>
+	);
+}
+
+function AppLoading() {
+	return (
+		<div className="flex flex-col gap-2 w-full justify-center items-center">
+			<img
+				className="w-[300px] animate-pulse"
+				src="/simmer-logo.svg"
+				alt="SIMMER logo"
+			/>
+			<div>
+				Loading...
+				<Spinner />
+			</div>
+		</div>
 	);
 }
