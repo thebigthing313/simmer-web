4556a2cb2bfe7089823f5b8abfffcb646b37cac7
thebigthing313 <adriankabigting@gmail.com>
2025-11-08 10:16:38 -0500

    fix: update bio assignment to handle potential undefined value

--- src/routes/(user)/profile.tsx
+++ src/routes/(user)/profile.tsx
@@ -51,7 +51,7 @@ function RouteComponent() {
 				user_id: data.user_id,
 				first_name: data.first_name,
 				last_name: data.last_name,
-				bio: data.bio ?? '',
+				bio: data.bio,
 				avatar_url: data.avatar_url,
 			});
 		}
