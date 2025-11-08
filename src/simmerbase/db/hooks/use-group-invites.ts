60d3570b9c395a3f31d7a54e81f3d6b12b007e2c
thebigthing313 <adriankabigting@gmail.com>
2025-11-08 10:09:11 -0500

    refactor: remove unused hooks

--- src/simmerbase/db/hooks/use-group-invites.ts
+++
@@ -1,6 +0,0 @@
-import { useLiveSuspenseQuery } from '@tanstack/react-db';
-import { group_invites } from '../collections/group_invites';
-
-export function useGroupInvites() {
-	return useLiveSuspenseQuery((q) => q.from({ group_invite: group_invites }));
-}
