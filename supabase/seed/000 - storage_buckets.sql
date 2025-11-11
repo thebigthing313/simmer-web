573dd18dcf319581e5872e54d30767a06ebaf388
thebigthing313 <adriankabigting@gmail.com>
2025-11-10 19:13:22 -0500

    remove seed files

--- supabase/seed/000 - storage_buckets.sql
+++
@@ -1,6 +0,0 @@
-insert into
-    storage.buckets (id, name, public, allowed_mime_types, file_size_limit)
-values
-    ('avatars', 'avatars', true, ARRAY['image/*'], 3145728),
-    ('group_images', 'group_images', true, ARRAY['image/*'], 10485760)
-on conflict (id) do nothing;
--- supabase/seed/002 - profiles.sql
+++
@@ -1,3 +0,0 @@
-insert into public.profiles (id, first_name, last_name, user_id)
-VALUES
-    ('10d84e43-7e7b-4992-8d13-221b9f2e8588', 'Adrian', 'Kabigting', '45626763-278d-47d5-b881-d2ebd0abe80b')
